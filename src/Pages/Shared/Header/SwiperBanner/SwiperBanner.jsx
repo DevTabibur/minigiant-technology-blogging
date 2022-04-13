b8ec06b0519df5446eb40848beacba9d04db27e1
DevTabibur <tobiburrohman2@gmail.com>
2022-04-14 01:57:42 +0600

    db updated and created

--- src/Pages/Shared/Header/SwiperBanner/SwiperBanner.jsx
+++ src/Pages/Shared/Header/SwiperBanner/SwiperBanner.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import './SwiperBanner.css';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./SwiperBanner.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import slideImg1 from "../../../../Assets/Images/mm-slide-2-1.jpg";
+import slideImg2 from "../../../../Assets/Images/mm-slide-2-2.jpg";
+import slideImg3 from "../../../../Assets/Images/mm-slide-2-3.jpg";
+
+// Import Swiper styles
+import "swiper/css";
 
 const SwiperBanner = () => {
-    return (
-        <div>
-            <h2>This is swiper</h2>
-        </div>
-    );
+  return (
+    <div className="swiper-banner-compo py-5">
+      <Container className="">
+        <Row>
+          <Col>
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={3}
+              onSlideChange={() => console.log("slide change")}
+              onSwiper={(swiper) => console.log(swiper)}
+            >
+              <SwiperSlide>
+                <img src={slideImg1} alt="" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className="d-block" src={slideImg2} alt="" />
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <img className="d-block" src={slideImg3} alt="" />
+              </SwiperSlide>
+            </Swiper>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default SwiperBanner;
