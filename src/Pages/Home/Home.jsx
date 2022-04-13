cd97c629bad1da1b6295a0bb71dc716aeab3c741
DevTabibur <tobiburrohman2@gmail.com>
2022-04-14 02:00:55 +0600

    updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,12 @@ import "./Home.css";
 import { Container } from "react-bootstrap";
 import SwiperBanner from "../Shared/Header/SwiperBanner/SwiperBanner";
 
+// this project is should be followed by 
+// https://atixscripts.info/demo/html/minimag/index-2.html
 const Home = () => {
   return (
     <>
-      <SwiperBanner />
+      {/* <SwiperBanner /> */}
       <Container>
         <h2>Home</h2>
       </Container>
